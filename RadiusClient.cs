73cf5d0ad7d39d7a53405a7469eb01dbff0ed37d
verne <verner@fortelius.net>
2017-08-09 22:38:38 +0300

    Send and receive asynchronously on same port
    requests and responses are matched together using identifier and remote host:port

--- RadiusClient.cs
+++ RadiusClient.cs
@@ -1,15 +1,18 @@
 ﻿using Flexinets.Radius.Core;
 using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Flexinets.Radius
 {
-    public class RadiusClient
+    public class RadiusClient : IDisposable
     {
         private readonly IPEndPoint _localEndpoint;
+        private readonly UdpClient _udpClient;
         private readonly RadiusDictionary _dictionary;
+        private readonly ConcurrentDictionary<(Byte identifier, IPEndPoint remoteEndpoint), TaskCompletionSource<UdpReceiveResult>> _pendingRequests = new ConcurrentDictionary<(Byte, IPEndPoint), TaskCompletionSource<UdpReceiveResult>>();
 
 
         /// <summary>
@@ -21,6 +24,8 @@ namespace Flexinets.Radius
         {
             _localEndpoint = localEndpoint;
             _dictionary = dictionary;
+            _udpClient = new UdpClient(_localEndpoint);
+            var receiveTask = StartReceiveLoopAsync();
         }
 
 
@@ -33,21 +38,19 @@ namespace Flexinets.Radius
         /// <returns></returns>
         public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint, TimeSpan timeout)
         {
-            var packetBytes = packet.GetBytes(_dictionary);
-            using (var udpClient = new UdpClient(_localEndpoint))
+            var packetBytes = packet.GetBytes(_dictionary);            
+            var responseTaskCS = new TaskCompletionSource<UdpReceiveResult>();
+            if (_pendingRequests.TryAdd((packet.Identifier, remoteEndpoint), responseTaskCS))
             {
-                await udpClient.SendAsync(packetBytes, packetBytes.Length, remoteEndpoint);
-
-                // todo use events to create a common udpclient for multiple packets to enable sending and receiving without blocking
-                var responseTask = udpClient.ReceiveAsync();
-
-                var completedTask = await Task.WhenAny(responseTask, Task.Delay(timeout));
-                if (completedTask == responseTask)
+                await _udpClient.SendAsync(packetBytes, packetBytes.Length, remoteEndpoint);
+                var completedTask = await Task.WhenAny(responseTaskCS.Task, Task.Delay(timeout));
+                if (completedTask == responseTaskCS.Task)
                 {
-                    return RadiusPacket.Parse(responseTask.Result.Buffer, _dictionary, packet.SharedSecret);
-                }                
-                throw new TaskCanceledException($"Receive timed out after {timeout}");
+                    return RadiusPacket.Parse(responseTaskCS.Task.Result.Buffer, _dictionary, packet.SharedSecret);
+                }
+                throw new InvalidOperationException($"Receive response for id {packet.Identifier} timed out after {timeout}");
             }
+            throw new InvalidOperationException($"There is already a pending receive with id {packet.Identifier}");
         }
 
 
@@ -60,6 +63,33 @@ namespace Flexinets.Radius
         public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint)
         {
             return await SendPacketAsync(packet, remoteEndpoint, TimeSpan.FromSeconds(3));
+        }
+
+
+        /// <summary>
+        /// Receive packets in a loop and complete tasks based on identifier
+        /// </summary>
+        /// <returns></returns>
+        private async Task StartReceiveLoopAsync()
+        {
+            while (true)    // Maybe this should be started and stopped when there are pending responses
+            {
+                try
+                {
+                    var response = await _udpClient.ReceiveAsync();
+                    if (_pendingRequests.TryRemove((response.Buffer[1], response.RemoteEndPoint), out var taskCS))
+                    {
+                        taskCS.SetResult(response);
+                    }
+                }
+                catch (ObjectDisposedException) { } // This is thrown when udpclient is disposed, can be safely ignored
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _udpClient?.Dispose();            
         }
     }
 }
