b5116166ad6a8cb116ec13521e1e11cc25b743fe
Verner Fortelius <verner@fortelius.net>
2024-11-27 22:04:50 +0200

    some refactoring and cleanup

--- Flexinets.Radius.RadiusClient/src/RadiusClient.cs
+++ Flexinets.Radius.RadiusClient/src/RadiusClient.cs
@@ -1,103 +1,117 @@
-﻿using Flexinets.Radius.Core;
-using System;
+﻿using System;
+using Flexinets.Radius.Core;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Flexinets.Radius
 {
     public class RadiusClient : IDisposable
     {
-        private readonly IPEndPoint _localEndpoint;
-        private readonly UdpClient _udpClient;
         private readonly IRadiusPacketParser _radiusPacketParser;
-        private readonly ConcurrentDictionary<(Byte identifier, IPEndPoint remoteEndpoint), TaskCompletionSource<UdpReceiveResult>> _pendingRequests = new ConcurrentDictionary<(Byte, IPEndPoint), TaskCompletionSource<UdpReceiveResult>>();
+        private readonly UdpClient _udpClient;
+        private Task? _receiveLoopTask;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private readonly
+            ConcurrentDictionary<(byte Identifier, IPEndPoint RemoteEndpoint), TaskCompletionSource<UdpReceiveResult>>
+            _pendingRequests =
+                new ConcurrentDictionary<(byte Identifier, IPEndPoint RemoteEndpoint),
+                    TaskCompletionSource<UdpReceiveResult>>();
 
 
         /// <summary>
         /// Create a radius client which sends and receives responses on localEndpoint
         /// </summary>
-        /// <param name="localEndpoint"></param>
-        /// <param name="dictionary"></param>
         public RadiusClient(IPEndPoint localEndpoint, IRadiusPacketParser radiusPacketParser)
         {
-            _localEndpoint = localEndpoint;
             _radiusPacketParser = radiusPacketParser;
-            _udpClient = new UdpClient(_localEndpoint);
-            var receiveTask = StartReceiveLoopAsync();
-        }
-
-
-        /// <summary>
-        /// Send a packet with specified timeout
-        /// </summary>
-        /// <param name="packet"></param>
-        /// <param name="remoteEndpoint"></param>
-        /// <param name="timeout"></param>
-        /// <returns></returns>
-        public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint, TimeSpan timeout)
-        {
-            var packetBytes = _radiusPacketParser.GetBytes(packet);
-            var responseTaskCS = new TaskCompletionSource<UdpReceiveResult>();
-            if (_pendingRequests.TryAdd((packet.Identifier, remoteEndpoint), responseTaskCS))
-            {
-                await _udpClient.SendAsync(packetBytes, packetBytes.Length, remoteEndpoint);
-                var completedTask = await Task.WhenAny(responseTaskCS.Task, Task.Delay(timeout));
-                if (completedTask == responseTaskCS.Task)
-                {
-                    return _radiusPacketParser.Parse(responseTaskCS.Task.Result.Buffer, packet.SharedSecret);
-                }
-                if (_pendingRequests.TryRemove((packet.Identifier, remoteEndpoint), out var taskCS))
-                {
-                    taskCS.SetCanceled();
-                }
-                throw new InvalidOperationException($"Receive response for id {packet.Identifier} timed out after {timeout}");
-            }
-            throw new InvalidOperationException($"There is already a pending receive with id {packet.Identifier}");
+            _udpClient = new UdpClient(localEndpoint);
         }
 
 
         /// <summary>
         /// Send a packet with default timeout of 3 seconds
         /// </summary>
-        /// <param name="packet"></param>
-        /// <param name="remoteEndpoint"></param>
-        /// <returns></returns>
-        public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint)
+        public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint) =>
+            await SendPacketAsync(packet, remoteEndpoint, TimeSpan.FromSeconds(3));
+
+
+        /// <summary>
+        /// Send a packet with specified timeout
+        /// </summary>
+        public async Task<IRadiusPacket> SendPacketAsync(IRadiusPacket packet, IPEndPoint remoteEndpoint,
+            TimeSpan timeout)
         {
-            return await SendPacketAsync(packet, remoteEndpoint, TimeSpan.FromSeconds(3));
+            // Start a receive loop before sending packet if one isnt already running to ensure we can receive the response
+            _receiveLoopTask ??= Task.Factory.StartNew(
+                StartReceiveLoopAsync,
+                _cancellationTokenSource.Token,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+
+            var completionSource = new TaskCompletionSource<UdpReceiveResult>();
+
+            if (_pendingRequests.TryAdd((packet.Identifier, remoteEndpoint), completionSource))
+            {
+                var bytes = _radiusPacketParser.GetBytes(packet);
+                await _udpClient.SendAsync(bytes, bytes.Length, remoteEndpoint);
+
+                if (await Task.WhenAny(completionSource.Task, Task.Delay(timeout)) == completionSource.Task)
+                {
+                    return _radiusPacketParser.Parse(
+                        completionSource.Task.Result.Buffer,
+                        packet.SharedSecret,
+                        packet.Authenticator);
+                }
+
+                if (_pendingRequests.TryRemove((packet.Identifier, remoteEndpoint), out var tcs))
+                {
+                    tcs.SetCanceled();
+                }
+
+                throw new InvalidOperationException(
+                    $"Receive response for id {packet.Identifier} timed out after {timeout}");
+            }
+
+            throw new InvalidOperationException($"There is already a pending receive with id {packet.Identifier}");
         }
 
 
         /// <summary>
         /// Receive packets in a loop and complete tasks based on identifier
         /// </summary>
-        /// <returns></returns>
         private async Task StartReceiveLoopAsync()
         {
-            while (true)    // Maybe this should be started and stopped when there are pending responses
+            while (!_cancellationTokenSource.IsCancellationRequested)
             {
                 try
                 {
                     var response = await _udpClient.ReceiveAsync();
-                    if (_pendingRequests.TryRemove((response.Buffer[1], response.RemoteEndPoint), out var taskCS))
+                    if (_pendingRequests.TryRemove((response.Buffer[1], response.RemoteEndPoint),
+                            out var tcs))
                     {
-                        taskCS.SetResult(response);
+                        tcs.SetResult(response);
                     }
                 }
-                catch (ObjectDisposedException)
+                catch (ObjectDisposedException) // This is thrown when udpclient is disposed, can be safely ignored
                 {
-                    // This is thrown when udpclient is disposed, can be safely ignored
-                    return;
                 }
             }
         }
 
 
+        /// <summary>
+        /// Dispose
+        /// </summary>
         public void Dispose()
         {
-            _udpClient?.Dispose();
+            GC.SuppressFinalize(this);
+            _cancellationTokenSource.Cancel();
+            _receiveLoopTask?.Dispose();
+            _udpClient.Dispose();
         }
     }
 }
